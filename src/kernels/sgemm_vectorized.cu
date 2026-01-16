9c2106fc96c85ef8e7718d55906bfd8deb78f1f8
Abhay Aysola <abhay.ayyasola@gmail.com>
2026-01-17 00:47:46 +0530

    feat(vectorized_kernel): optimized memory logic

--- src/kernels/sgemm_vectorized.cu
+++ src/kernels/sgemm_vectorized.cu
@@ -6,6 +6,7 @@ const int TILE_SIZE = 4;
 __host__ __device__ int cdiv(int a, int b) { return (a + b - 1) / b; }
 
 // multiplies two matrices M (mxn) and N (nxk) and stores the result in P (mxk)
+// TODO: it currently fails on matrix sizes that are not multiples of 4
 __global__ void sgemm_kernel(float *M, float *N, float *P, int m, int n, int k,
                              float alpha, float beta) {
   int bx = blockIdx.x;
@@ -15,8 +16,8 @@ __global__ void sgemm_kernel(float *M, float *N, float *P, int m, int n, int k,
   int row = blockDim.y * by + ty;
   int col = blockDim.x * bx + tx;
 
-  __shared__ float Ms[BLOCK_SIZE][BLOCK_SIZE];
-  __shared__ float Ns[BLOCK_SIZE][BLOCK_SIZE];
+  __shared__ float Ms[BLOCK_SIZE][BLOCK_SIZE + 4];
+  __shared__ float Ns[BLOCK_SIZE][BLOCK_SIZE + 4];
 
   float M_regs[TILE_SIZE];
   float N_regs[TILE_SIZE];
@@ -26,17 +27,17 @@ __global__ void sgemm_kernel(float *M, float *N, float *P, int m, int n, int k,
   for (int tile = 0; tile < tiles; tile++) {
     // cooperative load
     for (int i = 0; i < TILE_SIZE; i++) {
+      int s_row = ty * TILE_SIZE + i;
+      int m_row = row * TILE_SIZE + i;
       for (int j = 0; j < TILE_SIZE / 4; j++) {
         int s_col = tx * TILE_SIZE + j * 4;
-        int s_row = ty * TILE_SIZE + i;
-        int m_row = row * TILE_SIZE + i;
         int m_col = tile * BLOCK_SIZE + tx * TILE_SIZE + j * 4;
         if (m_row < m and m_col < n) {
           float4 tmp = reinterpret_cast<float4 *>(&M[m_row * n + m_col])[0];
-          Ms[s_col + 0][s_row] = tmp.x;
-          Ms[s_col + 1][s_row] = tmp.y;
-          Ms[s_col + 2][s_row] = tmp.z;
-          Ms[s_col + 3][s_row] = tmp.w;
+          Ms[s_row][s_col + 0] = tmp.x;
+          Ms[s_row][s_col + 1] = tmp.y;
+          Ms[s_row][s_col + 2] = tmp.z;
+          Ms[s_row][s_col + 3] = tmp.w;
         }
         int n_row = tile * BLOCK_SIZE + ty * TILE_SIZE + i;
         int n_col = col * TILE_SIZE + j * 4;
@@ -51,9 +52,10 @@ __global__ void sgemm_kernel(float *M, float *N, float *P, int m, int n, int k,
     }
 
     __syncthreads();
+
     for (int k = 0; k < BLOCK_SIZE; k++) {
       for (int i = 0; i < TILE_SIZE; i++) {
-        M_regs[i] = Ms[k][ty * TILE_SIZE + i];
+        M_regs[i] = Ms[ty * TILE_SIZE + i][k];
         N_regs[i] = Ns[k][tx * TILE_SIZE + i];
       }
 
@@ -66,11 +68,17 @@ __global__ void sgemm_kernel(float *M, float *N, float *P, int m, int n, int k,
     __syncthreads();
   }
   for (int i = 0; i < TILE_SIZE; i++) {
-    for (int j = 0; j < TILE_SIZE; j++) {
+    for (int j = 0; j < TILE_SIZE / 4; j++) {
       int p_row = row * TILE_SIZE + i;
-      int p_col = col * TILE_SIZE + j;
-      if (p_row < m and p_col < k)
-        P[p_row * k + p_col] = results_regs[i][j];
+      int p_col = col * TILE_SIZE + j * 4;
+      if (p_row < m and p_col < k) {
+        float4 tmp;
+        tmp.x = results_regs[i][j * 4 + 0];
+        tmp.y = results_regs[i][j * 4 + 1];
+        tmp.z = results_regs[i][j * 4 + 2];
+        tmp.w = results_regs[i][j * 4 + 3];
+        reinterpret_cast<float4 *>(&P[p_row * k + p_col])[0] = tmp;
+      }
     }
   }
 }
